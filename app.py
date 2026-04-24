8302cb3f57220e80a870f019a3f2f4c56a7517fc
Jinseon Lee <memilmuk82@gmail.com>
2026-04-24 10:25:47 +0900

    수행평가 예시

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+messages = []
+
+@app.route('/')
+def home():
+    return render_template('index.html', data=messages)
+
+@app.route('/send', methods=['POST'])
+def send():
+    skill = request.form.get('skill')
+    
+    messages.append(skill)
+    
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
